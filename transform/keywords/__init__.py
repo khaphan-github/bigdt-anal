6dafc8a0ceaadced3908286bd7af59e3929a34d0
khaphan-github <phanhoangkha01@gmail.com>
2026-05-30 16:57:16 +0700

    Remove keywork via python

--- transform/keywords/__init__.py
+++
@@ -1,9 +0,0 @@
-"""BigDT Transform Keywords - Trending keywords extraction"""
-__version__ = "0.1.0"
-
-from .trending_words_job import extract_keywords, run_job, tokenize
-
-__all__ = ["extract_keywords", "run_job", "tokenize"]
-
-
-
